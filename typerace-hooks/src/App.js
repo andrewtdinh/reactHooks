0d19be5041e7669c583e375f5fa25eeae0b5fa06
Andrew Dinh <adinh@sightmachine.com>
2019-08-25 21:20:10 -0700

    Add snippets and another hook for the current snippet

--- typerace-hooks/src/App.js
+++ typerace-hooks/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+  const [snippet, setSnippet] = useState('');
   const [userText, setUserText] = useState('');
 
   const updateUserText = event => {
