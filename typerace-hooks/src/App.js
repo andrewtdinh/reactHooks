e85d0574aaea6300f80d28e93a910097aef91f41
Andrew Dinh <adinh@sightmachine.com>
2019-08-25 20:58:02 -0700

    Update the input onChange function with updateUserText

--- typerace-hooks/src/App.js
+++ typerace-hooks/src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [userText, setUserText] = useState('');
+
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    console.log('current userText', userText);
+  }
+
   return (
     <div>
       <h2>Type Race</h2>
-      <input />
+      <input value={userText} onChange={updateUserText} />
     </div>
   );
 };
