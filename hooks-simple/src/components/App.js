d92d32760146914bc36dc379fe8e5403d6280dcb
Andrew Dinh <adinh@sightmachine.com>
2019-09-30 07:01:12 -0700

    Import and use ResourceList in App

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
 const App = () => {
   const [resource, setResource] = useState('Posts')
@@ -9,7 +10,7 @@ const App = () => {
         <button onClick={() => setResource('Posts')}>Posts</button>
         <button onClick={() => setResource('Todos')}>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   )
 }
