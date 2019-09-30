91f1e032e9ce8a647eafa39fca91034cf00e1f61
Andrew Dinh <adinh@sightmachine.com>
2019-09-29 22:17:36 -0700

    Solve for stopwatch using functional component with hook

--- myStopWatch/src/components/App.js
+++ myStopWatch/src/components/App.js
@@ -2,27 +2,36 @@ import React, { useState } from 'react';
 
 
 const App = () => {
-  let interval;
-  let [time, setTime] = useState(0);
-  let [running, setRunning] = useState(false);
-
-  const handleStartStop = (e) => {
-    if (!running) {
-      clearInterval(interval);
-      interval = setInterval(() => {
-        const currentTime = parseInt(document.querySelector('#time-display').innerHTML);
-        setTime(currentTime + 1)
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
-    setRunning(!running);
-  }
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [interval, setIntervalState] = useState();
 
   return (
     <div>
       <div>
-        <button onClick={handleStartStop}>
+        <button 
+          onClick={() => {
+            if (!running) {
+              if (interval) {
+                clearInterval(interval);
+                const currentInterval = setInterval(() => {
+                  const currentTime = parseInt(document.querySelector("#time-display").innerHTML);
+                  setTime(currentTime + 1);
+                }, 1000);
+                setIntervalState(currentInterval);
+              } else {
+                const currentInterval = setInterval(() => {
+                  const currentTime = parseInt(document.querySelector("#time-display").innerHTML);
+                  setTime(currentTime + 1);
+                }, 1000);
+                setIntervalState(currentInterval);
+              }
+            } else {
+              clearInterval(interval);
+            }
+            setRunning(!running);
+          }}
+        >
           {running ? 'Stop' : 'Start'}
         </button>
         <button onClick={() => setTime(0)}>Reset Time</button>
