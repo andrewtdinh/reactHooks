4cb12e252552acc51b9e7326e350afaa5db661c5
Andrew Dinh <adinh@sightmachine.com>
2019-10-01 22:14:57 -0700

    Bring in useEffect for when state is updated

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = (props) => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async () => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${props.resource}`);
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
     setResources(response.data);
   }
-  
+
+  useEffect(() => {
+    fetchResource(resource)
+  }, [])
+
   return (<div>{resources.length}</div>)
 }
 
